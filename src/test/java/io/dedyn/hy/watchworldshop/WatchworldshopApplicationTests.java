19ea523948a91835e824683b08063afd48d69962
khanghy1000 <46063255+khanghy1000@users.noreply.github.com>
2024-04-15 23:43:56 +0700

    feat: initial commit

---
+++ src/test/java/io/dedyn/hy/watchworldshop/WatchworldshopApplicationTests.java
@@ -0,0 +1,13 @@
+package io.dedyn.hy.watchworldshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatchworldshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
