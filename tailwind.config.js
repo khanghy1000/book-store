c7a99b32f4d877edf4584856313e5e75101f383d
khanghy1000 <46063255+khanghy1000@users.noreply.github.com>
2024-04-16 21:56:05 +0700

    chore: remove Preline UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,13 @@
 module.exports = {
     content: [
         "./src/main/resources/templates/**/*.{html,js}",
-        'node_modules/preline/dist/*.js',
     ],
     theme: {
         extend: {},
     },
     darkMode: 'selector',
     plugins: [
-        require('preline/plugin'),
+
     ],
 }
 
