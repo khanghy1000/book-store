53f5fde26c29bc1b011b18407a99ecd172db0594
khanghy1000 <46063255+khanghy1000@users.noreply.github.com>
2024-04-19 08:54:33 +0700

    chore(Category): remove unused validation

--- src/main/java/io/dedyn/hy/watchworldshop/entities/Category.java
+++ src/main/java/io/dedyn/hy/watchworldshop/entities/Category.java
@@ -26,7 +26,6 @@ public class Category {
     @Column(name = "name", nullable = false)
     private String name;
 
-    @Size(max = 255)
     @Column(name = "slug", nullable = false)
     private String slug;
 
