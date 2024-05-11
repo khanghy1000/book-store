38cdd92cf09824e44ef94a5401259221ab8e2ba8
khanghy1000 <46063255+khanghy1000@users.noreply.github.com>
2024-05-11 23:24:15 +0700

    feat: use lombok getter setter for classes

--- src/main/java/io/dedyn/hy/watchworldshop/entities/Category.java
+++ src/main/java/io/dedyn/hy/watchworldshop/entities/Category.java
@@ -4,12 +4,14 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.LinkedHashSet;
 import java.util.Set;
 
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Setter
 @Entity
