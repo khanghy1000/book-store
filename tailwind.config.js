4850b78eaa618de94d9cad3fab0870ee80a33de8
khanghy1000 <46063255+khanghy1000@users.noreply.github.com>
2024-05-13 00:13:48 +0700

    chore(tailwind): scan only html files

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-        "./src/main/resources/templates/**/*.{html,js}",
+        "./src/main/resources/templates/**/*.html",
     ],
     theme: {
         extend: {
