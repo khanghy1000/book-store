970b55bdbfd222ad214a9dc8099ff80012b2c0f4
khanghy1000 <46063255+khanghy1000@users.noreply.github.com>
2024-06-07 21:49:08 +0700

    feat: add order details page and allow user to change order status

--- src/main/java/io/dedyn/hy/watchworldshop/controller/OrderController.java
+++ src/main/java/io/dedyn/hy/watchworldshop/controller/OrderController.java
@@ -2,6 +2,7 @@ package io.dedyn.hy.watchworldshop.controller;
 
 import io.dedyn.hy.watchworldshop.entities.User;
 import io.dedyn.hy.watchworldshop.entities.order.Order;
+import io.dedyn.hy.watchworldshop.entities.order.OrderStatus;
 import io.dedyn.hy.watchworldshop.security.UserDetailsImpl;
 import io.dedyn.hy.watchworldshop.services.OrderService;
 import io.dedyn.hy.watchworldshop.services.UserService;
@@ -9,7 +10,11 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.time.LocalDateTime;
 
 @Controller
 @RequestMapping("/orders")
@@ -44,5 +49,64 @@ public class OrderController {
         model.addAttribute("order", order);
 
         return "order_details";
+    }
+
+    @PostMapping("/change-status")
+    public String changeStatus(@AuthenticationPrincipal UserDetailsImpl userDetails,
+                               Long orderId,
+                               OrderStatus status,
+                               RedirectAttributes redirectAttributes) {
+        Order order = orderService.findById(orderId);
+        if (order == null) {
+            return "redirect:/";
+        }
+
+        User user = userService.findByEmail(userDetails.getUsername());
+        if (user == null) {
+            return "redirect:/login";
+        }
+
+        if (status == order.getStatus()) {
+            redirectAttributes.addFlashAttribute("error", "Trạng thái đơn hàng không thay đổi.");
+            return "redirect:/orders/" + orderId;
+        }
+
+        if (status != OrderStatus.CANCELLED && user.getRole().getName().equals("Khách hàng")) {
+            return "redirect:/customer/orders";
+        }
+
+        if (status == OrderStatus.CANCELLED && user.getRole().getName().equals("Khách hàng") && !order.getCustomer().getId().equals(user.getId())) {
+            return "redirect:/customer/orders";
+        }
+
+        if (status == OrderStatus.CANCELLED && order.getStatus() == OrderStatus.DELIVERED) {
+            redirectAttributes.addFlashAttribute("error", "Không thể hủy đơn hàng đã giao.");
+            return "redirect:/orders/" + orderId;
+        }
+
+        if (status == OrderStatus.DELIVERED && order.getStatus() == OrderStatus.CANCELLED) {
+            redirectAttributes.addFlashAttribute("error", "Không thể đánh dấu đơn hàng đã hủy là đã giao.");
+            return "redirect:/orders/" + orderId;
+        }
+
+        if (status == OrderStatus.SHIPPING && (order.getStatus() == OrderStatus.CANCELLED || order.getStatus() == OrderStatus.DELIVERED)) {
+            redirectAttributes.addFlashAttribute("error", "Không thể đánh dấu đơn hàng đã hủy hoặc đã giao là đang giao.");
+            return "redirect:/orders/" + orderId;
+        }
+
+        if (status == OrderStatus.ORDERED && order.getStatus() != OrderStatus.ORDERED) {
+            redirectAttributes.addFlashAttribute("error", "Không thể đánh dấu đơn hàng đang giao/đã giao/đã huỷ là đã đặt.");
+            return "redirect:/orders/" + orderId;
+        }
+
+        order.setStatus(status);
+        if (order.getStatus() == OrderStatus.DELIVERED) {
+            order.setDeliverTime(LocalDateTime.now());
+        }
+
+        orderService.save(order);
+        redirectAttributes.addFlashAttribute("success", "Đã cập nhật trạng thái đơn hàng.");
+
+        return "redirect:/orders/" + orderId;
     }
 }
