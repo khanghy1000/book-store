aa25484fe481092faaa69746baeb02660172f4ef
khanghy1000 <46063255+khanghy1000@users.noreply.github.com>
2024-06-08 09:58:22 +0700

    fix: missing orders when order does not have any products

--- src/main/java/io/dedyn/hy/watchworldshop/repositories/OrderRepository.java
+++ src/main/java/io/dedyn/hy/watchworldshop/repositories/OrderRepository.java
@@ -11,36 +11,44 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
     @Query("""
         select distinct o from Order o
         left join o.orderDetails od
+        left join od.product p
         where o.customer.id = ?2
-        and (lower(concat(o.id, ' ', o.total, ' ',  o.fullName, ' ', o.ward, ' ', o.district, ' ', o.province, ' ', od.product.name))
-        like lower(concat('%', ?1, '%')))
+        and (lower(concat(o.id, ' ', o.total, ' ',  o.fullName, ' ', o.ward, ' ', o.district, ' ', o.province))
+        like lower(concat('%', ?1, '%'))
+        or lower(p.name) like lower(concat('%', ?1, '%')))
         """)
     Page<Order> findAllByKeywordAndCustomerId(String keyword, Long customerId, Pageable pageable);
 
     @Query("""
         select distinct o from Order o
         left join o.orderDetails od
+        left join od.product p
         where o.customer.id = ?2
         and o.status = ?3
-        and (lower(concat(o.id, ' ', o.total, ' ', o.fullName, ' ', o.ward, ' ', o.district, ' ', o.province, ' ', od.product.name))
-        like lower(concat('%', ?1, '%')))
+        and (lower(concat(o.id, ' ', o.total, ' ', o.fullName, ' ', o.ward, ' ', o.district, ' ', o.province))
+        like lower(concat('%', ?1, '%'))
+        or lower(p.name) like lower(concat('%', ?1, '%')))
         """)
     Page<Order> findAllByKeywordAndCustomerIdAndStatus(String keyword, Long customerId, OrderStatus status, Pageable pageable);
 
     @Query("""
         select distinct o from Order o
         left join o.orderDetails od
-        where lower(concat(o.id, ' ', o.total, ' ', o.fullName, ' ', o.ward, ' ', o.district, ' ', o.province, ' ', od.product.name))
+        left join od.product p
+        where lower(concat(o.id, ' ', o.total, ' ', o.fullName, ' ', o.ward, ' ', o.district, ' ', o.province))
         like lower(concat('%', ?1, '%'))
+        or lower(p.name) like lower(concat('%', ?1, '%'))
         """)
     Page<Order> findAllByKeyword(String keyword, Pageable pageable);
 
     @Query("""
         select distinct o from Order o
         left join o.orderDetails od
+        left join od.product p
         where o.status = ?2
-        and (lower(concat(o.id, ' ', o.total, ' ', o.fullName, ' ', o.ward, ' ', o.district, ' ', o.province, ' ', od.product.name))
-        like lower(concat('%', ?1, '%')))
+        and (lower(concat(o.id, ' ', o.total, ' ', o.fullName, ' ', o.ward, ' ', o.district, ' ', o.province))
+        like lower(concat('%', ?1, '%'))
+        or lower(p.name) like lower(concat('%', ?1, '%')))
         """)
     Page<Order> findAllByKeywordAndStatus(String keyword, OrderStatus status, Pageable pageable);
 }
